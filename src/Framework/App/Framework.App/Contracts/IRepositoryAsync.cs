6416ca55b66debede54bb5f9c6414526e67c85e1
Daniel WOAGOU <daniwx@gmail.com>
2024-02-17 21:02:44 +0000

    coding Repository and IUnitOfWork

---
+++ src/Framework/App/Framework.App/Contracts/IRepositoryAsync.cs
@@ -0,0 +1,25 @@
+﻿using Framework.Domain.Contracts;
+using Framework.Domain.Models;
+
+namespace Framework.App.Contracts
+{
+    public interface IRepositoryAsync<T, in TId> where T : class, IEntity<TId>
+    {
+        IQueryable<T> Entities { get; }
+
+        Task<T> GetByIdAsync(TId id);
+
+        Task<List<T>> GetAllAsync();
+
+        Task<List<T>> GetPagedDataAsync(int pageNumber, int pageSize);
+
+        Task<T> AddAsync(T entity);       
+
+        Task UpdateAsync(T entity);
+        Task BulkUpdateAsync(IList<T> entity);
+        Task DeleteAsync(T entity);
+        Task BulkDeleteAsync(IList<T> entity);
+    }
+   
+
+}
---
+++ src/Framework/App/Framework.App/Contracts/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Framework.Domain.Models;
+
+namespace Framework.App.Contracts
+{
+    public interface IUnitOfWork<TId> : IDisposable
+    {
+        IRepositoryAsync<T, Guid> RepositoryAsync<T>() where T : AuditableEntity;
+
+        Task<int> Commit(CancellationToken cancellationToken);
+
+        Task<int> CommitAndRemoveCache(CancellationToken cancellationToken, params string[] cacheKeys);
+
+        Task Rollback();
+    }
+}
