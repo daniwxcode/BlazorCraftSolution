38147a8fd524acbf84a844baa18bf6b50168ea4a
Daniel WOAGOU <daniwx@gmail.com>
2024-02-19 10:16:06 +0000

    Creating Framework services

---
+++ src/Framework/Shared/Framework.Shared/Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Framework.Shared.Models
+{
+    public class User
+    {
+        public required string FirstName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public string CreatedBy { get; set; } = string.Empty;
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        public string? LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
